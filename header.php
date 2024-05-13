99b02d87b9e590f4ac0fa7ceb02c49b066fd3faa
hendriaprianto99 <hendriaprianto99@gmail.com>
2024-05-13 23:10:30 +0700

    Reale Finale

--- header.php
+++ header.php
@@ -12,24 +12,35 @@
 	<script type="text/javascript" src="tag.js"></script>  
 	<script type="text/javascript" src="kalender/calendar.js"></script>  
 	<link href="kalender/calendar.css" rel="stylesheet" type="text/css" media="screen"> 
+    
+    <!-- Custom CSS -->               
+    <link href="custom.css" rel="stylesheet"> 
+
     <!-- Bootstrap Core CSS -->               
     <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"> 
+    
     <!-- MetisMenu CSS -->
     <link href="bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">   
+    
     <!-- Timeline CSS -->  
     <link href="dist/css/timeline.css" rel="stylesheet">  
+    
     <!-- Custom CSS -->  
-    <link href="dist/css/sb-admin-2.css" rel="stylesheet"> 
+    <link href="dist/css/sb-admin-2.css" rel="stylesheet"> a
+    
     <!-- Morris Charts CSS -->         
     <link href="bower_components/morrisjs/morris.css" rel="stylesheet">  
+    
     <!-- Custom Fonts -->          
     <link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"> 
+    
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->     
     <!-- WARNING: Respond.js doesnt work if you view the page via file:// -->  
     <!--[if lt IE 9]>                                                             
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>  
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script> 
-    <![endif]-->                                   
+    <![endif]-->    
+                                   
 <link href="standar.css" rel="stylesheet" type="text/css">   
 <!-- calendar -->                         
 <script src="php_calendar/scripts.js" type="text/javascript"></script>  
